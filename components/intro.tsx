fe3bb3c04bbbe46292967538ca2491d2caa41ceb
Aldeev Nurgali <84390242+aldeevN@users.noreply.github.com>
2023-11-16 16:05:32 +0300

    Update intro.tsx

--- components/intro.tsx
+++ components/intro.tsx
@@ -62,7 +62,7 @@ export default function Intro() {
         initial={{ opacity: 0, y: 100 }}
         animate={{ opacity: 1, y: 0 }}
       >
-        <span className="font-bold">Hello, I'm Aldeev Nurgali.</span> I'm a{" "}
+        <span className="font-bold">Hello, I&aposm Aldeev Nurgali.</span> I&aposm a{" "}
         <span className="font-bold">front-end developer</span>{" "}
         I enjoy building <span className="italic">sites & apps</span>. My focus is{" "}
         <span className="underline">React (Next.js)</span>.
